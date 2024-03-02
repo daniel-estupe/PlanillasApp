3b8906a8a2e4db43e93eacec762e85818619e27d
Daniel Estupe <estupe.gonzalez@gmail.com>
2024-03-02 11:07:45 -0600

    Create unit testing scenarios

---
+++ PlanillaApi/PlanillaApi.UnitTests/Services/EmpleadosServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanillaApi.UnitTests.Services
+{
+    [TestClass]
+    public class EmpleadosServiceTests
+    {
+        [TestMethod]
+        public void CrearEmpleado_NoHayPlazaDisponible_NoDebePermitirElRegistro()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void CrearEmpleado_ExcedeTechoSalarial_NoDebePermitirElRegistro()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void CrearEmpleado_HayPlazaDisponibleYNoExcedeRangoSalarial_DebePermitirElRegistro()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
